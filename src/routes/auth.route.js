53fcc31ea7d60278835f630bb5983e55f6509fa5
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-22 19:53:56 +0700

    add fix forgot and reset password

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 const auth = require("express").Router();
 
-const {validEmail, check, passwordConfirmation} = require("../middlewares/validator.middleware");
+const {validEmail, check} = require("../middlewares/validator.middleware");
 
 auth.post("/login", validEmail, check, require("../controllers/auth.controller").login);
 auth.post("/register", require("../controllers/auth.controller").register);
