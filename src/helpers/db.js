a1da632d3ca5a99ff3a9f9459305a41cd9578038
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-27 14:41:51 +0700

    fix database

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg");
 
 const db = new Pool({
-  connectionString: "postgresql://postgres:1@localhost:5432/postgres?schema=public"
+  connectionString: "postgresql://postgres:1@localhost:5432/db?schema=public"
 });
 
 module.exports = db;
